ddec4a6787c025ee6fc42309031d347e53806b05
rexxnor <rexxnor@brief.li>
2022-10-10 08:24:13 +0200

    added csrf example

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 use rocket_contrib::templates::Template;
 use serde::{Serialize};
+use rocket_csrf::CsrfToken;
 
 mod routes;
 mod dto;
@@ -17,12 +18,15 @@ fn not_found(req: &rocket::Request) -> String {
 #[derive(Serialize)]
 pub struct Renderer {
 	pub xss_test: String,
+	pub csrf_token: String,
 }
 
 #[get("/")]
-fn root() -> Template {
+fn root(csrf_token: CsrfToken) -> Template {
+	let authenticity_token: String = csrf_token.authenticity_token();
 	let test = Renderer{
 		xss_test: String::from("</h1><script>alert(\"No...\");</script>"),
+		csrf_token: authenticity_token,
 	};
 	Template::render("home", test)
 }
@@ -35,7 +39,9 @@ fn main() {
 			routes::users,
 			routes::user_insert,
 			routes::user_get,
+			routes::login,
 		])
+		.attach(rocket_csrf::Fairing::default())
 		.attach(Template::fairing())
 		.launch();
 }
--- src/routes.rs
+++ src/routes.rs
@@ -1,11 +1,18 @@
-
 use diesel::prelude::*;
+
+use rocket::request::{Form, FromForm};
+use rocket_csrf::CsrfToken;
+use rocket::response::{Flash, Redirect};
 
 use rocket_contrib::json::Json;
 use crate::orm::get_connection;
 
 use crate::dto;
 use crate::schema::*;
+
+fn not_found(req: &rocket::Request) -> String {
+	format!("Something bad happened! Did you really mean '{}'?", req.uri())
+}
 
 #[get("/users")]
 pub fn users() -> Json<Vec<dto::User>> {
@@ -15,6 +22,24 @@ pub fn users() -> Json<Vec<dto::User>> {
 		.expect(format!("Error while loading users from Database, so sorry...").as_str());
 
 	Json(users)
+}
+
+#[derive(FromForm)]
+pub struct LoginForm {
+	csrf_token: String,
+	username: String,
+	password: String,
+}
+
+#[post("/login", data = "<form>")]
+pub fn login(csrf_token: CsrfToken, form: Form<LoginForm>) -> Flash<Redirect> {
+	if let Err(_) = csrf_token.verify(&form.csrf_token) {
+		return Flash::error(Redirect::to("/"), "invalid authenticity token");
+	}
+	Flash::success(
+		Redirect::to("/api/users"),
+		format!("Login succeeded: {:#?}", form.username),
+	)
 }
 
 #[post("/user/insert", format = "application/json", data = "<new_user>")]
